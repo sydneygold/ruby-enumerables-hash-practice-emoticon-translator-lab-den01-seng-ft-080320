6ec49927251d4f19ccb775dd259b28df9f49f2f4
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-23 23:58:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,7 +29,7 @@ def get_english_meaning(file, japanese_emoticon)
 library = load_library(file)
 library.each do |key, value|
   if japanese_emoticon == key[:japanese]
-    return key[:english]
+    binding.pry
     end
   end
   p "Sorry, that emoticon was not found"
