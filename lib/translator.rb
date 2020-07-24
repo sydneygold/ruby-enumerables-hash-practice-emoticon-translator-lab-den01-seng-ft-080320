3b955ccb56ed17a9d8e16c7f325ee6752f7606db
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-24 00:09:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,7 +29,7 @@ def get_english_meaning(file, japanese_emoticon)
 library = load_library(file)
 library.each do |key, value|
   if japanese_emoticon == value[:japanese]
-    return 
+    return library
     #binding.pry
     end
   end
